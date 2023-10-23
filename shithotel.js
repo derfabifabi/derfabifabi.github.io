6ff57f5aaccd57f57ff6ef02c3ca4fe3814a4385
Fabian Barth <fabianbarth@Fabians-MBP.fritz.box>
2023-10-23 21:10:25 +0200

    second guitar added (C) from first 4 takes, vocal cutting, new delay return track drum fills fixed bit mixing

---
+++ shithotel.js
@@ -0,0 +1,56 @@
+const songs = {
+  severedheads: {
+    name: 'shithotel',
+    title: 'Shit Hotel',
+    takes: 1,
+  },
+};
+
+
+addSongsToDom(songs);
+
+function createTake(song, index) {
+  const { name, title, takes } = song;
+  const el = document.createElement('li');
+  el.classList.add('flex', 'flex-col', 'gap-y-1');
+  const innerEl = `
+      <h4 class="text-xs flex-shrink-0">Take ${index + 1}</h4>
+      <audio controls class="w-full">
+        <source
+          src="export/${name}/edit/${name}_take_0${index + 1}.mp3"
+          type="audio/mpeg"
+        />
+      </audio>
+    `;
+  el.innerHTML = innerEl;
+  return el;
+}
+
+function createTakes(song) {
+  const el = document.createElement('ul');
+  el.classList.add('flex', 'flex-col', 'gap-y-3', 'py-6');
+  for (let index = 0; index < song.takes; index++) {
+    el.appendChild(createTake(song, index));
+  }
+  return el;
+}
+
+function createSong(song) {
+  const el = document.createElement('li');
+  const details = document.createElement('details');
+  details.innerHTML = `<summary class="sticky top-0 cursor-pointer bg-white px-2 py-1 text-3xl">${song.title}</summary>`;
+  details.appendChild(createTakes(song));
+  el.appendChild(details);
+  return el;
+}
+
+function addSongsToDom(songs) {
+  Object.keys(songs).forEach((song) => {
+    document
+      .getElementById('songList')
+      .appendChild(createSong(songs[song]));
+  });
+};
+
+
+
